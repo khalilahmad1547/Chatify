f9217fe1e318e061543551231b313dbeb02840d9
Abdur Rahman <khalil.ahmad1547@gmail.com>
2023-04-08 23:19:24 +0500

    update & destroy user actions are added.

--- backend/app/controllers/api/v0/users_controller.rb
+++ backend/app/controllers/api/v0/users_controller.rb
@@ -14,9 +14,21 @@ class Api::V0::UsersController < ApplicationController
     render json: @user, serializer: UserSerializer, status: :ok
   end
 
-  def update; end
+  def update
+    if @user.update user_params
+      render json: @user, serializer: UserSerializer, status: :ok
+    else
+      render json: { errors: @user.errors.messages }, status: :bad_request
+    end
+  end
 
-  def destroy; end
+  def destroy
+    if @user.destroy
+      render json: @user, serializer: UserSerializer, status: :ok
+    else
+      render json: { errors: @user.errors.messages }, status: :bad_request
+    end
+  end
 
   private
 
@@ -25,6 +37,6 @@ class Api::V0::UsersController < ApplicationController
   end
 
   def find_user
-    @user = User.find_by(id: params[:id])
+    @user = User.find_by!(id: params[:id])
   end
 end
